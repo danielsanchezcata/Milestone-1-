fb2761dbdbb42c6fb6b81ce7c52f82fc6352f448
mercyakinyele <ma980@cam.ac.uk>
2022-02-10 14:54:38 +0000

    1D TEST

--- test_geo.py
+++ test_geo.py
@@ -1,4 +1,4 @@
-from floodsystem.geo import stations_within_radius
+from floodsystem.geo import stations_by_river, stations_within_radius
 from floodsystem.geo import rivers_by_station
 from floodsystem.stationdata import build_station_list
 
@@ -7,9 +7,18 @@ def test_stations_within_radius():
     for station in stations:
         if station.name == 'Bin Brook':
             station_cam = station
+            break
     assert station_cam
 
 def test_rivers_by_station():
     rivers  = rivers_by_station()
     for river in rivers:
         assert river[1] >= 0 
+
+def test_stations_by_river():
+    stations = stations_by_river()
+    for station in stations:
+        if station.name == 'Airmyn':
+            station_cam = station
+        break
+    assert station_cam
