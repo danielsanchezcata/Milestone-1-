52a6d453cf5d50b042f1b93cce170b1f565f1235
Daniel Sanchez <ds987@cam.ac.uk>
2022-02-10 14:00:02 +0000

    delete test

--- test_1B.py
+++
@@ -1,5 +0,0 @@
-from floodsystem.stationdata import build_station_list
-from floodsystem.geo import stations_by_distance
-def test_distance():
-    distance = stations_by_distance()
-    assert distance >= 0 
