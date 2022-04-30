a54c8abc745f197e5e6b9682c164e1828354a612
mercyakinyele <ma980@cam.ac.uk>
2022-04-30 19:45:25 +0100

    task 2c and test 2c

--- test_flood.py
+++ test_flood.py
@@ -1,4 +1,4 @@
-from floodsystem.flood import stations_level_over_threshold
+from floodsystem.flood import stations_level_over_threshold, stations_high_rel_level
 from floodsystem.stationdata import build_station_list, update_water_levels
 
 def test_stations_level_over_threshold():
@@ -9,4 +9,12 @@ def test_stations_level_over_threshold():
     s_list = []
     for s in range(len(station)):
         s_list.append((str(station[s][0].name), str(station[s][1])))
-    assert len(s_list) >= 1
+    assert len(s_list) >= 0
+
+def test_stations_highest_rel_level():
+    stations= build_station_list()
+    s_list = []
+    for s in stations_high_rel_level(stations,10):
+        relative_level=s.latest_level-s.typical_range[1]
+        s_list.append((s.name, relative_level))
+    assert len(s_list) >= 0
